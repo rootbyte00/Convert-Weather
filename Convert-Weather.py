950c33c26b5a7bf6e6a8d27c1a0e2ce6ceecba0d
rootbyte00 <rootbyte00@users.noreply.github.com>
2017-04-16 21:23:16 +0300

    2.0

--- Convert-Weather.py
+++ Convert-Weather.py
@@ -1,4 +1,5 @@
 # -*-coding:utf-8-*-
+import string
 def celsius_to_fehrenheit():
 	print "Convert Celsius To Ferenheit"
 	celsius = int(raw_input("Type A Celsius Degree: "))
@@ -6,8 +7,8 @@ def celsius_to_fehrenheit():
 	fehrenheit = "°F"
 	print "Its %s%s" % (celsius, fehrenheit)
 	print ""
-
 	return
+	
 def fehrenheit_to_celsius():
 	print "Convert Fehrenheit To Celsius"
 	fehrenheit = int(raw_input("Type A Fehrenheit Degree: "))
@@ -18,16 +19,64 @@ def fehrenheit_to_celsius():
 	print ""
 	
 	return 	
+
+def celsius_to_kelvin():
+	print "Convert Celsius To Kelvin"
+	celsius = int(raw_input("Type A Celsius Degree: "))
+	celsius = celsius + 273.15
+	kelvin = "K"
+	print "Its %s%s" % (celsius, kelvin)
+	print ""
+	return
+
+def kelvin_to_celsius():
+	print "Convert Kelvin To Fehrenheit"
+	kelvin = int(raw_input("Type A Kelvin Degree: "))
+	kelvin = kelvin - 273.15
+	celsius = "°C"
+	print "Its %s%s" % (kelvin, celsius)
+	print ""
+	return
+
+def fehrenheit_to_kelvin():
+	print "Convert Fehrenheit To Kelvin"
+	fehrenheit = int(raw_input("Type A Fehrenheit Degree: "))
+	fehrenheit = fehrenheit + 459.67
+	fehrenheit = fehrenheit * 5/9
+	kelvin = "K"
+	print "Its %s%s" % (fehrenheit, kelvin)
+	print ""
+	return
+
+def kelvin_to_fehrenheir():
+	print "Convert Fehrenheit To Kelvin"
+	kelvin = int(raw_input("Type A Kelvin Degree: "))
+	kelvin = kelvin * 9/5
+	kelvin = kelvin - 459.67
+	fehrenheit = "°F"
+	print "Its %s%s" % (kelvin, fehrenheit)
+	print ""
+	return
+
 def main():
-	menu = [celsius_to_fehrenheit, fehrenheit_to_celsius,]
+	menu = [celsius_to_fehrenheit, fehrenheit_to_celsius, celsius_to_kelvin, kelvin_to_celsius, fehrenheit_to_kelvin, kelvin_to_fehrenheit]
 	while(True):
 		print "Select Option: "
 		print "1\tCelsius To Fehrenheit"
 		print ""
 		print "2\tFehrenheit To Celsius"
+		print ""
+		print "3\tCelsius To Kelvin"
+		print ""
+		print "4\tKelvin To Celsius"
+		print ""
+		print "5\tFehrenheit To Kelvin"
+		print ""
+		print "5\tKelvin To Fehrenheit"
 		choice = int(raw_input("Selection: "))
 		choice -= 1
 		menu[choice]()
 			
+
 	return
 main()
